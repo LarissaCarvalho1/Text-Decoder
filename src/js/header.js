386842d5720d2b0a8bfc3f875f026f4fdbe212b1
Larissa Carvalho <carvalholarissa530@gmail.com>
2024-01-25 15:54:02 -0300

    Initial project

---
+++ src/js/header.js
@@ -0,0 +1,39 @@
+const html = document.documentElement;
+const checkbox = document.querySelector('.js-checkbox');
+
+function toggleTheme() {
+    html.classList.toggle("dark", checkbox.checked);
+
+    if (checkbox.checked) {
+        saveTheme("light");
+        console.log("light");
+    } else {
+        saveTheme("dark");
+        console.log("dark");
+    }
+}
+
+function saveTheme(theme) {
+    localStorage.setItem("theme", theme);
+}
+
+function initTheme() {
+    const theme = localStorage.getItem("theme");
+
+    if (theme === "light") {
+        checkbox.checked = false;
+        html.classList.remove("dark");
+    } else {
+        checkbox.checked = true;
+        html.classList.add("dark");
+    }
+}
+
+// Chama a função initTheme para configurar o tema inicial
+initTheme();
+
+// Adiciona um ouvinte de evento para o checkbox
+checkbox.addEventListener('change', toggleTheme);
+
+
+
